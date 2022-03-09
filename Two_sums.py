c4c87402358bf18a1733cb44441d22814a555554
shruthivenkat <amritashruthi@gmail.com>
2022-03-08 18:19:41 -0800

    two sums

--- Two_sums.py
+++ Two_sums.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_hash = {}
+        num_hash = {}
         for i, num in enumerate(nums):
-            potentialMatch = target - num
-            if potentialMatch in nums_hash:
-                return [nums_hash[potentialMatch], i]
-            nums_hash[num] = i
+            potential_match = target - num
+            if potential_match in num_hash:
+                return [num_hash[potential_match], i]
+            num_hash[num] = i
