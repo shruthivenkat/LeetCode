d25c1e89ab12cb950f880519f2379884fafb557b
shruthivenkat <amritashruthi@gmail.com>
2022-04-19 18:26:22 -0700

    Day 1 : Delete unwanted

--- Two_sums.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_hash = {}
-        for i, num in enumerate(nums):
-            potential_match = target - num
-            if potential_match in num_hash:
-                return [num_hash[potential_match], i]
-            num_hash[num] = i
