7435a3e7aa2dd7707994a5cfb05ac615f02176f4
shruthivenkat <amritashruthi@gmail.com>
2022-06-17 09:26:41 -0700

    starting fresh

--- Trees/binary_inorder.py
+++
@@ -1,7 +0,0 @@
-# 94. Binary Tree Inorder Traversal
-
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-    	if root is None:
-    		return []
-    	return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
--- Trees/binary_postorder.py
+++
@@ -1,7 +0,0 @@
-# 145. Binary Tree Postorder Traversal
-
-class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-    	if root is None:
-    		return []
-    	return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
